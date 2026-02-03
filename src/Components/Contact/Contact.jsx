538a6bac124e9bede20024f0616ce6af73c65779
Abdujabbor <mahamatsiddiqovandujabbor@gmail.com>
2026-02-03 17:26:25 +0500

    Update

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Contact.css'
-import Img from './img/Img 2.png'
+import Img from './img/Img (2).png'
 import Circle from './img/Oval Copy 4.png'
 const Contact = () => {
   return (
@@ -8,7 +8,24 @@ const Contact = () => {
     <div className="contact">
         <div className="container">
             <div className="contact-container">
-                <img src="" alt="" />
+                <img className='contact-img' src={Img} alt="" />
+                <img src={Circle} alt="" className="contact-circle" />
+            <div className="contact-texts">
+              <h1 className="contact-text">
+                Get started now
+              </h1>
+              <p className="contact-info">
+                Create custom landing pages with Omega that converts <br /> 
+                more visitors than any website. With lots of unique blocks, <br /> 
+                you can easily build a page without coding.
+              </p>
+              <form className='contact-form'>
+                <input className='contact-inp' type="email" placeholder='Email' />
+                <button className="contact-btn">
+                Get Started
+                </button>
+              </form>
+            </div>
             </div>
         </div>
     </div>
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <> 
+      <div className="footer">
+        <div className="container">
+            <div className="footer-container">
+
+            </div>
+        </div>
+      </div>   
+    </>
+  )
+}
+
+export default Footer
