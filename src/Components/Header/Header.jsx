7dd9dd4f098cd085380a18ebc19c1c08e4dcc441
Abdujabbor <mahamatsiddiqovandujabbor@gmail.com>
2026-02-04 20:11:34 +0500

    Header  Tuzatildi

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,47 @@
 import React from 'react'
 import './Header.css'
 import bg from './img/BG (1).png'
+import Circle from './img/Oval.png'
+import Up from './img/2.png'
+import axios from 'axios'
+import { useState } from 'react'
 const Header = () => {
+    const [email, setEmail] = useState("");
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    if (!email) {
+      alert("Email kiriting ❗");
+      return;
+    }
+
+    const token = "8586580877:AAEazv2Y3aPuc9jNS6_ApbFDYgWfpiFf23k";
+    const chat_id = 5258999486;
+    const url = `https://api.telegram.org/bot${token}/sendMessage`;
+
+    const text = `📧 Yangi obunachi:\nEmail: ${email}`;
+
+    axios.post(url, {
+      chat_id: chat_id,
+      text: text,
+    })
+      .then(() => {
+        alert("Ma'lumot Telegram botga yuborildi ✅");
+        setEmail("");
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("Xatolik yuz berdi ❌");
+      });
+  };
     return (
         <>
             <div className="header">
                 <div className="container">
                     <div className="header-container">
+                        <img src={Circle} alt="" className="circle-header" />
+                        <img src={Up} alt="" className="header-up" />
                         <div className="header-texts" data-aos="fade-up"
                             data-aos-duration="3000">
                             <h1 className="header-text">
@@ -15,9 +50,13 @@ const Header = () => {
                             <p className="header-info">
                                 Create custom landing pages with Omega that convert more <br /> visitors than any website. With lots of unique blocks, you can <br /> easily build a page without coding.
                             </p>
-                            <form action="">
-                                <input type="email" placeholder='Email Address' />
-                                <button className='header-button'>
+                            <form action="" onSubmit={sendMessage}>
+                                <input type="email" placeholder='Email Address'
+                                
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required />
+                                <button type='submit' className='header-button'>
                                     <div class="svg-wrapper-1">
                                         <div class="svg-wrapper">
                                             <svg
